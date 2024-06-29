2dab54416f91b19ae63aa78aaf052b2a4d91e589
Dasha <grace-kramm666@gmail.com>
2024-06-29 19:26:08 +0600

    добавила модели + сделала миграцию + зарегистрировала в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Product, Category
+
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'description']
+    fields = ['name', 'description']
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'category', 'added_to', 'price']
+    list_display_links = ['id', 'name']
+    list_filter = ['category']
+    search_fields = ['name', 'description', 'category', 'price']
+    fields = ['name', 'description', 'category', 'added_to', 'price', 'image']
+    readonly_fields = ['added_to']
+
+
+admin.site.register(Product, ProductAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, verbose_name="Наименование", unique=True)
+    description = models.CharField(max_length=70, null=True, blank=True, verbose_name="Описание")
+
+    def __str__(self):
+        return f"{self.pk}) {self.name}"
+
+    class Meta:
+        db_table = "categories"
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, verbose_name="Наименование")
+    description = models.CharField(max_length=70, null=True, blank=True, verbose_name="Описание")
+    category = models.ForeignKey('webapp.Category', on_delete=models.RESTRICT, null=False, blank=False,
+                                 verbose_name="Категория", related_name='products')
+    added_to = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время добавления")
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False, verbose_name="Стоимость")
+    image = models.URLField(null=False, blank=False, verbose_name="Изображение")
+
+    def __str__(self):
+        return f"{self.pk}) {self.name} - {self.price}"
+
+    class Meta:
+        db_table = "products"
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
