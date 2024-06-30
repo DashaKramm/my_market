d836b87155f4235f108f475b609a3e24ee1323e8
Dasha <grace-kramm666@gmail.com>
2024-06-30 16:18:17 +0600

    создала view + url + шаблон для страницы с добавлением категории

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
-from webapp.models import Product
+from webapp.models import Product, Category
 
 
 # Create your views here.
@@ -13,3 +13,18 @@ def products_view(request):
 def product_view(request, *args, pk, **kwargs):
     product = get_object_or_404(Product, pk=pk)
     return render(request, "product_view.html", context={"product": product})
+
+
+def category_add_view(request):
+    if request.method == "GET":
+        return render(request, "category_add_view.html")
+    else:
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        if not description:
+            description = None
+        Category.objects.create(
+            name=name,
+            description=description,
+        )
+        return redirect('products_view')
